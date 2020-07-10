fe975c6921deb004493ec1f3c1c0220d53b5dafa
ishmael <ishmael@qoreit.com>
2020-07-10 23:43:43 +0800

    Display all question with pagination

---
+++ resources/views/questions/index.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">All Questions</div>
+
+                <div class="card-body">
+                    @forelse($questions as $question)
+                        <div class="media">
+                            <div class="media-body">
+                                <h3 class="mt-0">{{ $question->title }}</h3>
+                                {{ \Illuminate\Support\Str::limit($question->body, 200) }}
+                            </div>
+                        </div>
+                        <hr>
+                    @empty
+                    <p>No data found</p>
+                    @endforelse
+
+                    <div class="mx-auto">
+                        {{ $questions->links() }}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
